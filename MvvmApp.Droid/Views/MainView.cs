10af9e813bc86a9725c0783f6dfd02f75e9aee42
Leonardo Bononi <leonardo.bononi@programmers.com.br>
2018-06-15 16:01:30 -0300

    Added visual studio app center packages

--- MvvmApp.Droid/Views/MainView.cs
+++ MvvmApp.Droid/Views/MainView.cs
@@ -1,6 +1,9 @@
 using Acr.UserDialogs;
 using Android.App;
 using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using MvvmCross.Droid.Views;
 
 namespace MvvmApp.Droid.Views
@@ -12,6 +15,7 @@ namespace MvvmApp.Droid.Views
     {
         protected override void OnCreate(Bundle bundle)
         {
+            AppCenter.Start("ca454f95-7ba8-4780-ada6-9b44c7f26b33", typeof(Analytics), typeof(Crashes));
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.MainView);
             UserDialogs.Init(this);
